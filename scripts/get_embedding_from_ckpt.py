5086148204113f5501283470ed06601db56b869a
Nguyễn Việt Thanh Duy <67935775+thanhduy1842001@users.noreply.github.com>
2024-05-28 20:06:04 +0700

    Update get_embedding_from_ckpt.py

--- scripts/get_embedding_from_ckpt.py
+++ scripts/get_embedding_from_ckpt.py
@@ -13,12 +13,13 @@ from biomedkg.configs import data_settings, train_settings, node_settings, kge_s
 from biomedkg.modules.utils import find_device
 from lightning import seed_everything
 
+
 def parse_opt():
     parser = argparse.ArgumentParser()
-    
+   
     parser.add_argument(
-            '--ckpt', 
-            type=str, 
+            '--ckpt',
+            type=str,
             default=None,
             required=False,
             help="Path to ckpt file")
@@ -26,25 +27,30 @@ def parse_opt():
     return opt
 
 
+
+
 def main(
         ckpt:str,
 ):
     assert os.path.exists(ckpt)
 
+
     device = find_device()
 
+
     seed_everything(train_settings.SEED)
-    
+   
     dir_name = os.path.dirname(ckpt)
     json_file = os.path.join(dir_name, "node_mapping.json")
+    model_name = os.path.basename(dir_name).split("_")[0]
+    modality_transform = os.path.basename(dir_name).split("_")[2]
+
 
     with open(json_file, "r") as file:
         mapping_dict = json.load(file)
 
+
     if re.search(r"gcl", dir_name):
-
-        model_name = os.path.basename(dir_name).split("_")[0]
-
         if model_name == "dgi":
             model = gcl_module.DGIModule.load_from_checkpoint(ckpt)
         elif model_name == "grace":
@@ -54,12 +60,15 @@ def main(
         else:
             raise NotImplementedError
 
+
         node_type = dir_name.split("_")[1]
+
 
         if node_type == "gene":
             process_node = ['gene/protein']
         else:
             process_node = [node_type]
+
 
         data_module = PrimeKGModule(
             data_dir=data_settings.DATA_DIR,
@@ -67,39 +76,42 @@ def main(
             process_edge_lst=data_settings.EDGES_LST,
             batch_size=train_settings.BATCH_SIZE,
             encoder=EncodeNodeWithModality(
-                entity_type=node_type, 
+                entity_type=node_type,
                 embed_path=os.path.join(os.path.dirname(data_settings.DATA_DIR),"embed")
                 )
         )
 
-        data_module.setup(stage="split", embed_dim=node_settings.PRETRAINED_NODE_DIM)
 
+        data_module.setup(stage="split", embed_dim=node_settings.PRETRAINED_NODE_DIM)
     else:
         model = kge_module.KGEModule.load_from_checkpoint(ckpt)
+
 
         # Decide node intialize method
         node_init_method = model.hparams.node_init_method
 
+
         if node_init_method == "gcl":
             encoder = EncodeNode(
-                embed_path=os.path.join(os.path.dirname(data_settings.DATA_DIR), "gcl_embed")
+                embed_path=os.path.join(os.path.dirname(data_settings.DATA_DIR), f"gcl_embed/{model_name}_{modality_transform}")
                 )
         elif node_init_method == "llm":
             encoder = EncodeNodeWithModality(
-                entity_type=list(data_settings.NODES_LST), 
+                entity_type=list(data_settings.NODES_LST),
                 embed_path=os.path.join(os.path.dirname(data_settings.DATA_DIR), "embed"),
                 )
         elif node_init_method == "random":
             encoder = None
         else:
             raise NotImplementedError
-        
+       
         if node_init_method == "gcl" or node_init_method == "random":
             embed_dim = node_settings.GCL_TRAINED_NODE_DIM
         elif node_init_method == "llm":
             embed_dim = node_settings.PRETRAINED_NODE_DIM
         else:
             embed_dim = None
+
 
         # Setup data module
         data_module = PrimeKGModule(
@@ -112,46 +124,59 @@ def main(
             encoder=encoder,
         )
 
+
         data_module.setup(stage="split", embed_dim=embed_dim)
+
 
     model = model.to(device)
     model.eval()
 
+
     subgraph_loader = data_module.subgraph_dataloader()
+
 
     idx_to_node_dict = {v: k for k, v in mapping_dict.items()}
 
+
     node_embedding_mapping = dict()
+
 
     for batch in tqdm(subgraph_loader):
         x = batch.x.to(device)
 
+
         with torch.no_grad():
-            if hasattr(batch, "edge_type"):
-                out = model(x, batch.edge_index.to(device), batch.edge_type.to(device))
-            else:
+            if re.search(r"gcl", dir_name):
                 out = model(x, batch.edge_index.to(device))
+            else:
+                out = model(x, batch.edge_index.to(device), batch.edge_type.to(device))
+
 
         for node_id, embed in zip(batch.n_id[:batch.batch_size].tolist(), out[:batch.batch_size].detach().cpu().numpy()):
             node_embedding_mapping[idx_to_node_dict[node_id]] = embed
 
+
     if re.search(r"gcl", dir_name):
-        save_dir = os.path.join(os.path.dirname(data_settings.DATA_DIR), "gcl_embed")
+        save_dir = os.path.join(os.path.dirname(data_settings.DATA_DIR), f"gcl_embed/{model_name}_{modality_transform}")
     else:
         save_dir = os.path.join(os.path.dirname(data_settings.DATA_DIR), "kge_embed")
 
+
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    
+   
     save_file_name = os.path.join(
         save_dir,
         os.path.basename(os.path.dirname(ckpt)) + ".pickle",
     )
 
+
     with open(save_file_name, "wb") as file:
         pickle.dump(node_embedding_mapping, file, protocol=pickle.HIGHEST_PROTOCOL)
-    
+   
     print(f"Save {save_file_name} completed")
+
+
 
 
 if __name__ == "__main__":
